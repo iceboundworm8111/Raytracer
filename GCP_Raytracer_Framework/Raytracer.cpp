c56bf617f8545c32af74f81e3f9d410f52b014d2
Nathan <nathanbailey2992@gmail.com>
2024-11-11 13:50:13 +0000

    Depth Perception

--- GCP_Raytracer_Framework/Raytracer.cpp
+++ GCP_Raytracer_Framework/Raytracer.cpp
@@ -1,19 +1,29 @@
 #include "Raytracer.h"
 
-
 glm::vec3 Raytracer::TraceRay(Ray ray)
 {
-	
+	glm::vec3 PixelColour(0.3f, 0.3f, 0.3f);
+	glm::vec3 CloseCollidePosition;
+
+	float CloseDepth = 10000000.0f;
+
 	for (int i = 0; i < sphere.size(); i++)
 	{
 		glm::vec3 CollidePosition;
+
 		if(sphere[i]->RayCollide(ray,CollidePosition))
 		{
-			return sphere[i]->ShadePosition(CollidePosition);
+			float Depth = glm::length(CollidePosition - ray.mOrigin);
+			if (Depth < CloseDepth)
+			{
+				CloseDepth = Depth;
+				CloseCollidePosition = CollidePosition;
+				PixelColour = sphere[i]->ShadePosition(CollidePosition);
+			}
 		}
 
 	}
 
-	return glm::vec3(0.3f, 0.3f, 0.3f);
+	return PixelColour;
 
 }
